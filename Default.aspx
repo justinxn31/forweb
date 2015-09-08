221cfd710bed83e61b51094e2a16252952ca0126
justinxn31 <justinxn31@hotmail.com>
2015-09-08 14:46:20 +0800

    first test

---
+++ Default.aspx
@@ -0,0 +1,165 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <%-- <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css "/>
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+  <link rel="stylesheet" href="/resources/demos/style.css "/>
+  <script>
+      $(function () {
+          $("#datepicker").datepicker();
+      });
+  </script>--%>
+  <%-- <link type="text/css" href="css/smoothness/jquery-ui-1.7.1.custom.css" rel="stylesheet" />
+     <script src="_scripts/jquery-1.3.2.min.js" type="text/javascript"></script>
+     <script src="_scripts/jquery-ui-1.7.1.custom.min.js" type="text/javascript"></script>
+  <script type="text/javascript">
+      $(function () {
+          $("#txtDate").datepicker();
+      });
+
+	</script>--%>
+
+
+
+    <style type="text/css">
+        
+        .MyCalendar .ajax__calendar_container {
+    border:1px solid #646464;
+    background-color: lemonchiffon;
+    color: red;
+}
+          .word1 {
+        font-family:微軟正黑體;
+
+        }
+
+        .word {
+        font-family:微軟正黑體;
+        color:white;
+        }
+                .word4 {
+        font-family:微軟正黑體;
+        color:red;
+        font-size:14px;
+
+        }
+           .word3 {
+        font-family:微軟正黑體;
+        font-size:16px;
+        font-weight:bold;
+        color:black;
+        text-align:center;
+        }
+        .auto-style1 {
+            width: 300px;
+            height: 60px;
+        }
+
+        .bcolor {
+        color:#646464;
+        font-weight:bold;
+        font-size:12px;
+        }
+        .align1 {
+        text-align:center
+        }
+
+      body {
+          font-family:微軟正黑體;
+background: rgb(249,252,247); /* Old browsers */
+/* IE9 SVG, needs conditional override of 'filter' to 'none' */
+background: url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/Pgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgdmlld0JveD0iMCAwIDEgMSIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+CiAgPGxpbmVhckdyYWRpZW50IGlkPSJncmFkLXVjZ2ctZ2VuZXJhdGVkIiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgeDE9IjAlIiB5MT0iMTAwJSIgeDI9IjEwMCUiIHkyPSIwJSI+CiAgICA8c3RvcCBvZmZzZXQ9IjAlIiBzdG9wLWNvbG9yPSIjZjlmY2Y3IiBzdG9wLW9wYWNpdHk9IjEiLz4KICAgIDxzdG9wIG9mZnNldD0iMTAwJSIgc3RvcC1jb2xvcj0iI2QwZDZjZiIgc3RvcC1vcGFjaXR5PSIxIi8+CiAgPC9saW5lYXJHcmFkaWVudD4KICA8cmVjdCB4PSIwIiB5PSIwIiB3aWR0aD0iMSIgaGVpZ2h0PSIxIiBmaWxsPSJ1cmwoI2dyYWQtdWNnZy1nZW5lcmF0ZWQpIiAvPgo8L3N2Zz4=);
+background: -moz-linear-gradient(45deg,  rgba(249,252,247,1) 0%, rgba(208,214,207,1) 100%); /* FF3.6+ */
+background: -webkit-gradient(linear, left bottom, right top, color-stop(0%,rgba(249,252,247,1)), color-stop(100%,rgba(208,214,207,1))); /* Chrome,Safari4+ */
+background: -webkit-linear-gradient(45deg,  rgba(249,252,247,1) 0%,rgba(208,214,207,1) 100%); /* Chrome10+,Safari5.1+ */
+background: -o-linear-gradient(45deg,  rgba(249,252,247,1) 0%,rgba(208,214,207,1) 100%); /* Opera 11.10+ */
+background: -ms-linear-gradient(45deg,  rgba(249,252,247,1) 0%,rgba(208,214,207,1) 100%); /* IE10+ */
+background: linear-gradient(45deg,  rgba(249,252,247,1) 0%,rgba(208,214,207,1) 100%); /* W3C */
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f9fcf7', endColorstr='#d0d6cf',GradientType=1 ); /* IE6-8 fallback on horizontal gradient */
+
+
+
+
+background-repeat : no-repeat; 
+background-position: center;
+background-size:cover;
+}
+
+        .round {
+    border: 1px solid;
+    border-radius: 5px;
+        
+        }
+
+    </style>
+
+    <script type="text/javascript">
+        function ConfirmMe() {
+            if (Page_ClientValidate()) {
+                return confirm('確定送出嗎?');
+            }
+            else {
+                return false;
+            }
+        }
+</script>
+
+
+
+</head>
+<body >
+    <form id="form1" runat="server" >
+        <br/>
+        <div class="align1">
+           <img alt="report" class="auto-style1" src="img/headers.png" />
+            </div>
+     
+       
+        <div  align="center">
+            <br/>
+                 <p class="word3">
+        帳號：<asp:TextBox ID="tbid" runat="server" CssClass="round" Height="18px" Width="150px"></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator9"  ControlToValidate="tbid" runat="server" ErrorMessage="*" ForeColor="Red"></asp:RequiredFieldValidator>
+                        <%--<asp:TextBox ID="TextBox1" runat="server" Width="1px" Visible="false" Text="@excelsiorgroup.com.tw"></asp:TextBox>--%>
+
+    </p>
+    <p class="word3">
+        密碼：<asp:TextBox ID="tbpd" runat="server" TextMode="Password" CssClass="round" Height="18px" Width="150px"></asp:TextBox>
+            <asp:Regularexpressionvalidator id="nameRegex" runat="server" 
+        ControlToValidate="tbpd" 
+        ValidationExpression="^([a-zA-Z0-9]{1,40})+$" 
+        ErrorMessage="*" ForeColor="Red">
+  </asp:Regularexpressionvalidator>
+    <br/>
+   </p>
+          
+            <p>
+  
+        <asp:Button ID="Button2" runat="server" Text="登入" OnClick="Button2_Click" CssClass="word1" Height="26px" Width="90px" />
+        <asp:Label ID="Label6" runat="server"></asp:Label>
+ </p>
+             <br/>
+
+            </div>
+         
+        <p></p>
+        <p></p>
+        <p></p>
+        <br/>
+         <br/>
+
+      <div class="bcolor" align="center">  Copyright © 2014 Excelsior Biopharma Inc. All Rights Reserved.
+          </div>
+               <br/>
+         <br/>
+               <br/>
+         <br/>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Data.SqlClient;
+using System.DirectoryServices;
+
+
+
+
+ 
+public partial class _Default : System.Web.UI.Page
+{
+   Datacon1 dataconn = new Datacon1();
+   Datacon dataconn1 = new Datacon();
+
+    protected void Page_Load(object sender, EventArgs e)
+   {
+       UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+     
+       Session["ID"] = this.tbid.Text;
+       Session["PW"] = this.tbpd.Text;
+//       HttpCookie cookieID = new HttpCookie("ID");
+//       cookieID.Value = tbid.Text;
+//       Response.AppendCookie(cookieID);
+//HttpCookie cookiePW = new HttpCookie("PW");
+//cookiePW.Value = tbpd.Text;
+//       cookieID.Expires = DateTime.Now.AddDays(1);//cookie時間設定 範例為存活時間為創建後的1年
+
+//       Response.AppendCookie(cookiePW);
+       //this.Label3.Text = Session["ID"].ToString();
+
+
+        //if (!IsPostBack)
+        //{
+          
+        //    ///////////////////////////
+        //    //SqlConnection con = dataconn.getcon();
+        //    //SqlDataAdapter myadapter1 = new SqlDataAdapter("select * from sales ORDER BY ID DESC", con);
+        //    //DataTable dt = new DataTable();
+        //    ////foreach (DataTable table in dt.Tables){
+        //    ////  DataRow[] row3 = dt.Tables[0].Select();
+
+
+
+        //    //dt.Rows.Add(dt.NewRow());
+        //    //dt.Rows.Add(dt.NewRow());
+        //    //dt.Rows.Add(dt.NewRow());
+        //    //dt.Rows.Add(dt.NewRow());
+        //    //dt.Rows.Add(dt.NewRow());
+        //    //dt.Rows.Add(dt.NewRow());
+        //    //dt.Rows.Add(dt.NewRow());
+
+     
+        //    //GridView1.DataSource = dt;
+        //    //GridView1.DataBind();
+
+
+
+
+        //    //dataconn.ecadabindinfostring(GridView1, "select * from sales ORDER BY ID DESC", "ID");
+
+
+        //    for (int i = 0; i < 7; i++)
+        //    {
+        //        DropDownList dl1 = (DropDownList)GridView1.Rows[i].FindControl("DropdownList1");
+
+        //        dataconn.ecDropDownList(dl1, "select * from time", "name", "id");
+
+        //        DropDownList dl2 = (DropDownList)GridView1.Rows[i].FindControl("DropdownList2");
+        //        dataconn.ecDropDownList(dl2, "select * from area", "area", "id");
+
+        //        DropDownList dl3 = (DropDownList)GridView1.Rows[i].FindControl("DropdownList3");
+        //        dataconn.ecDropDownList(dl3, "select * from division", "division", "id");
+
+        //        DropDownList dl4 = (DropDownList)GridView1.Rows[i].FindControl("DropdownList4");
+        //        dataconn.ecDropDownList(dl4, "select * from title", "title", "id");
+
+        //        DropDownList dl5 = (DropDownList)GridView1.Rows[i].FindControl("DropdownList5");
+        //        dataconn.ecDropDownList(dl5, "select * from level1", "level1", "id");
+
+        //        DropDownList dl6 = (DropDownList)GridView1.Rows[i].FindControl("DropdownList6");
+        //        dataconn.ecDropDownList(dl6, "select * from mproduct", "mproduct", "id");
+
+        //        DropDownList dl7 = (DropDownList)GridView1.Rows[i].FindControl("DropdownList7");
+        //        dataconn.ecDropDownList(dl7, "select * from product1", "product1", "id");
+
+        //        DropDownList dl8 = (DropDownList)GridView1.Rows[i].FindControl("DropdownList8");
+        //        dataconn.ecDropDownList(dl8, "select * from product2", "product2", "id");
+
+        //    }
+        //    //////////////////////////////////////////
+            
+
+
+
+
+        //}
+    }
+
+
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+
+
+            this.getcom(2);
+        
+        
+        
+    }
+    //==============================登录控制============================
+    private void getcom(int i)
+    {
+
+        SqlConnection con = dataconn1.getcon();
+        con.Open();
+        SqlCommand com = con.CreateCommand();
+      
+        switch (i)
+        {  
+            
+          
+            case 1:
+                 com.CommandText = "select count(*) from admin where employeen='" + tbid.Text + "' and ID='" + tbpd.Text + "' and rank='1'";
+                int count3 = Convert.ToInt32(com.ExecuteScalar());
+                if (count3 > 0)
+                {
+                    Session["Login"] = "OK";
+              
+                    Application["ID"] = tbid.Text;
+                    Application["PWD"] = tbpd.Text;
+                    Response.Redirect("adminh1.aspx");
+                }
+                  else
+                {
+                    Response.Write("<script lanuage=javascript>alert('此帳號為管理者帳號，請先確定所選部門是否正確。在確定ID或密碼輸入是否有誤!');location='javascript:history.go(-1)'</script>");
+                    return;
+                }
+                break;
+
+            case 2:
+              
+                com.CommandText = "select  *  from employee where eme='" + tbid.Text + "' and password='" + tbpd.Text + "'";
+
+                //int count2 = Convert.ToInt32(com.ExecuteScalar());
+                SqlDataReader lrd = com.ExecuteReader();
+
+
+
+
+              if ( lrd.Read())
+                {
+
+                    //HttpCookie cookieu_name = new HttpCookie("u_name");
+                    //cookieu_name.Value = lrd["employeen"].ToString();
+                    //Response.Cookies.Add(cookieu_name);
+
+                    Session["u_name"] = lrd["employeen"].ToString();
+                    Session["u_rank"] = lrd["rank"].ToString();
+                    Session["eme"] = lrd["eme"].ToString();
+                    if (lrd["eme"].ToString() == "adminh")
+                    {
+                        Session["Login"] = "OK";
+                        Session["h"] = "h";
+                  
+                        Application["ID"] = tbid.Text;
+                        Application["PWD"] = tbpd.Text;
+                        Response.Redirect("adminh1.aspx");
+
+                    }
+                    else if( lrd["eme"].ToString() == "admins")
+                    {
+                        Session["s"] = "s";
+                        Session["Login"] = "OK";
+                        Application["ID"] = tbid.Text;
+                        Application["PWD"] = tbpd.Text;
+                        Response.Redirect("adminh1.aspx");
+                    }
+                    else if (lrd["eme"].ToString() == "human")
+                    {
+                        Session["Login"] = "OK";
+                        Application["ID"] = tbid.Text;
+                        Application["PWD"] = tbpd.Text;
+                        Response.Redirect("human1.aspx");
+                    }
+                    else if (lrd["rank"].ToString() == "1" )
+                    {
+                        //Application["d2"] = "1";
+                      //Session["Login"] = "OK";
+                      //Session["SelTitle"] = "健康管理事業部";
+                      //HttpCookie cookied2 = new HttpCookie("d2");
+                      //cookied2.Value = "1";
+                      //HttpCookie cookieSelTitle = new HttpCookie("SelTitle");
+                      //cookieSelTitle.Value = "健康管理事業部";
+                      //Response.Cookies.Add(cookieSelTitle);
+
+                      //string loginID = this.tbid.Text.Trim();
+                      //  HttpCookie cookieID = new HttpCookie("ID",loginID);
+                  //    cookieID.Value = tbid.Text;
+                     // Response.Cookies.Add(cookieID);
+                      //HttpCookie aCookie = new HttpCookie("ID1");
+                      //aCookie.Value = tbid.Text;
+                      //aCookie.Expires = DateTime.Now.AddDays(1);
+                      //Response.Cookies.Add(aCookie);
+
+                     
+
+
+                      //HttpCookie cookiePW = new HttpCookie("PW");
+                      //cookiePW.Value = tbpd.Text;
+                      //cookieID.Expires = DateTime.Now.AddDays(1);//cookie時間設定 範例為存活時間為創建後的1年
+             
+                      //Response.Cookies.Add(cookiePW);
+
+                        //Application["ID"] = tbid.Text;
+                        //Application["PWD"] = tbpd.Text;
+                        Application["d2"] = "1";
+                        Session["Login"] = "OK";
+                        Session["SelTitle"] = "健康管理事業部";
+
+                        Application["ID"] = tbid.Text;
+                        Application["PWD"] = tbpd.Text;
+                        Response.Redirect("breport.aspx");
+
+
+
+                        Response.Redirect("breport.aspx");
+                    }
+                    else if (lrd["rank"].ToString() == "2")
+                    {
+                        Application["d2"] = "2";
+                        Session["SelTitle"] = "特殊醫療事業部";
+                        Session["Login"] = "OK";
+                        Application["ID"] = tbid.Text;
+                        Application["PWD"] = tbpd.Text;
+                        Response.Redirect("sreport.aspx");
+                    }
+                }
+                else
+                {
+                    Response.Write("<script lanuage=javascript>alert('ID或密碼有誤!');location='javascript:history.go(-1)'</script>");
+                    return;
+                }
+                break;
+
+
+
+            case 3:
+                com.CommandText = "select count(*) from admin where employeen='" + tbid.Text + "' and ID='" + tbpd.Text + "' and rank='2'";
+                int count4 = Convert.ToInt32(com.ExecuteScalar());
+                if (count4 > 0)
+                {
+                    Session["Login"] = "OK";
+                    Application["ID"] = tbid.Text;
+                    Application["PWD"] = tbpd.Text;
+                    Response.Redirect("admins.aspx");
+                }
+                else
+                {
+                    Response.Write("<script lanuage=javascript>alert('此帳號為管理者帳號，請先確定所選部門是否正確。在確定ID或密碼輸入是否有誤!');location='javascript:history.go(-1)'</script>");
+                    return;
+                }
+                break;
+
+            case 4:
+                com.CommandText = "select count(*) from admin where employeen='" + tbid.Text + "' and ID='" + tbpd.Text + "' and rank='3'";
+                int count5 = Convert.ToInt32(com.ExecuteScalar());
+                if (count5 > 0)
+                {
+                    Session["Login"] = "OK";
+                    Application["ID"] = tbid.Text;
+                    Application["PWD"] = tbpd.Text;
+                    Response.Redirect("human.aspx");
+                }
+                else
+                {
+                    Response.Write("<script lanuage=javascript>alert('此帳號為管理者帳號，請先確定所選部門是否正確。在確定ID或密碼輸入是否有誤!');location='javascript:history.go(-1)'</script>");
+                    return;
+                }
+                break;
+
+
+
+
+
+
+
+
+        }
+        con.Close();
+    }
+    }
